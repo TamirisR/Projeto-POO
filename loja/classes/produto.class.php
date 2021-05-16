d7716243732898686a7fea9936a380a135270a87
TamirisR <Tamis_rodrigues@hotmail.com>
2021-05-15 21:30:42 -0300

    solid aula 04-05

--- loja/classes/produto.class.php
+++ loja/classes/produto.class.php
@@ -2,13 +2,14 @@
 
 require_once 'conexao.class.php';
 
-Class Produto extends Banco  {
+class Produto extends Banco
+{
 
-    
+
     protected $id;
-	protected $nome;
+    protected $nome;
     protected $preco;
-    protected $qtde;
+    protected $quantidade;
     protected $imagem;
     protected $categoria;
 
@@ -18,46 +19,51 @@ Class Produto extends Banco  {
         parent::__construct();
     }
 
-    public function setDados(array $dados){
+    public function setDados(array $dados)
+    {
         $this->nome = $dados[0] ?? null;
         $this->preco = $dados[1] ?? null;
-        $this->qtde = $dados[2] ?? null;
+        $this->quantidade = $dados[2] ?? null;
         $this->imagem = $dados[3] ?? null;
         $this->categoria = $dados[4] ?? null;
 
         return $this->inserir();
-             
     }
-    public function inserir(){
+    public function inserir()
+    {
 
         $stmt = $this->dns->prepare('INSERT INTO produtos (nome, preco, quantidade, imagem, categoria) VALUES (:nome, :preco, :quantidade, :imagem, :categoria)');
-    
-            if( $stmt->execute([':nome' => $this->nome,':preco' => $this->preco,':quantidade' => $this->qtde, ':imagem' => $this->imagem, ':categoria' => $this->categoria ])){
-    
-                return true;
-            }
+
+        if ($stmt->execute([':nome' => $this->nome, ':preco' => $this->preco, ':quantidade' => $this->quantidade, ':imagem' => $this->imagem, ':categoria' => $this->categoria])) {
+
+            return true;
+        }
     }
-    public function getId($se){
-      
+}
+class MostrarConteudo extends Produto
+{
+    public function getId($se)
+    {
+
         $stmt = $this->dns->prepare("SELECT idProduto FROM produtos WHERE nome  == '{$se}'");
-  
-          $stmt->execute();
-          $s=$stmt->fetchAll();
-          $this->id=$s[0][0];
-          
-  }
-  public function apagar($id){
 
-     $this->dns->query("DELETE FROM produtos WHERE idProduto ='{$id}'");
-    
-  }
+        $stmt->execute();
+        $s = $stmt->fetchAll();
+        $this->id = $s[0][0];
+    }
+    public function apagar($id)
+    {
 
-  public function mostrar(){
+        $this->dns->query("DELETE FROM produtos WHERE idProduto ='{$id}'");
+    }
 
-    $stmt = $this->dns->prepare('SELECT * FROM produtos');
+    public function mostrar()
+    {
 
-		$stmt->execute();
+        $stmt = $this->dns->prepare('SELECT * FROM produtos');
 
-		return $stmt->fetchAll();
-  }
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
 }
