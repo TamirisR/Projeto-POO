f531df7a7adb1f811787910f129a8b24edf2d6e9
TamirisR <Tamis_rodrigues@hotmail.com>
2021-05-19 00:23:29 -0300

    continuação do solid

---
+++ loja/classes/transporte.class.php
@@ -0,0 +1,39 @@
+<?php
+abstract class Entregas 
+{
+    public function entregaErrada(){}
+    abstract public function entregaSucesso();
+}
+
+class Correios extends Entregas 
+{
+    public function ligentregaSucessoar()
+    {
+        echo 'Correios entregou';
+    } 
+}
+
+class AmazonPrime extends Entregas 
+{
+    public function ligar()
+    {
+        echo 'Amazon entregou';
+    }
+}
+
+class MercadoLivre extends Entregas 
+{
+    public function ligar()
+    {
+        echo 'Mercado Livre entregou';
+    }
+}
+
+class RotaEntrega
+{
+    public function rota(Entregar  $entregar)
+    {
+        $entregar ->entregaSucesso();
+        $entregar ->entregaErrada();
+    }
+}
