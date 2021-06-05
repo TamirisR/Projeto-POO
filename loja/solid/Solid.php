1a1b84053d3e68e553d54d34a5d72276a08142ca
TamirisR <Tamis_rodrigues@hotmail.com>
2021-06-04 22:26:05 -0300

    continuação do solid

--- loja/solid/Solid.php
+++ loja/solid/Solid.php
@@ -48,3 +48,83 @@ class AuthApi extends Auth
         return ['auth' => true, 'status' => 200];
     }
 }
+
+?>
+<!-- Continuação do Solid com outros exemplos -->
+
+
+<?php
+
+
+interface Livro{
+    function read();
+}
+
+
+class PDFlivro implements Livro{
+ 
+    function read() {
+        return "lendo um livro em pdf.";
+    }
+}
+
+
+class Livroleitura {
+ 
+    private $livro;
+ 
+    function __construct(Livro $livro) {
+        $this->livro = $livro;
+    }
+ 
+    function read() {
+        return $this->livro->read();
+    }
+ 
+}
+
+
+$livro = new PDFlivro();
+$leitura = new Livroleitura($b);
+ 
+$leitura->read();
+
+?>
+<!-- outro exemplo -->
+
+<?php
+
+interface TrabalharInterface
+{
+    public  function trabalho();
+}
+
+interface DormiInterface
+{
+    public  function  dormi();
+}
+
+
+class Trabalhador implements TrabalharInterface, DormiInterface
+{
+
+    public  function trabalho()
+    {
+        var_dump('Trabalhar agora');
+    }
+    public  function  dormi()
+    {
+        var_dump('dormi');
+    }
+
+}
+
+class RobotWorker implements TrabalharInterface
+{
+
+    public  function trabalho()
+    {
+        var_dump('Trabalhar agora');
+    }
+
+}
